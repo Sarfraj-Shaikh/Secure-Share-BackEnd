c126c84813eb12701ff2a551c1a0ac98855f285d
Sarfraj Shaikh <work.shaikhmosarfraj@gmail.com>
2026-09-17 15:07:31 +0530

    Commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: process.env.FRONTEND_URL, // frontend URL
     credentials: true
 }));
 
