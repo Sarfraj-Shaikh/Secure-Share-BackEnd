81c2af07fd2bdfdce5cc800f48655eff1c863984
Sarfraj Shaikh <work.shaikhmosarfraj@gmail.com>
2026-09-19 10:38:59 +0530

    Commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -193,7 +193,7 @@ const verifyEmail = async (req, res) => {
         const token = jwt.sign(
             { id: user._id, role: user.role },
             process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRE, }
+            { expiresIn: '1m', }
         );
 
         // Mail transporter
@@ -222,7 +222,7 @@ const verifyEmail = async (req, res) => {
         );
 
         await user.save();
-        
+
         return res.status(200).json({
             success: true,
             message: "Verification Email Sent Successfully"
