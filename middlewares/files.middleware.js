a7e5a64d16d29e2595521d099f010c40cd08602d
Sarfraj Shaikh <work.shaikhmosarfraj@gmail.com>
2026-09-24 12:46:57 +0530

    Commit

--- middlewares/files.middleware.js
+++ middlewares/files.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 const multerUpload = multer({
     storage,
     limits: {
-        fileSize: 10 * 1024 * 1024, // 10 MB
+        fileSize: 1 * 1024 * 1024, // 1 MB
     },
 });
 
