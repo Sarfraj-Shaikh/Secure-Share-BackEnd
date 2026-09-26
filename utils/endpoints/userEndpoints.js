d25d572a990e12cc4882e9a541b1435a3ef11481
Sarfraj Shaikh <work.shaikhmosarfraj@gmail.com>
2026-09-26 15:01:55 +0530

    Commit

--- utils/endpoints/userEndpoints.js
+++ utils/endpoints/userEndpoints.js
@@ -40,8 +40,8 @@ export const userEnpoints = (app) => {
 
     app.post("/api/share-file", roleValidation, shareFileVal, shareFile);
     app.get("/api/share-file", roleValidation, fetchSharedFile);
-    
+
     app.get("/api/download-file/:id", roleValidation, getSharedFile);
-    app.post("/api/download-file", roleValidation, downloadSharedFile);
+    app.post("/api/download-file/:id", roleValidation, downloadSharedFile);
 
 }
